98f2842c038f6fb00b8140ac55c7efa1312ad32b
Ethel <34084693+Ethel173@users.noreply.github.com>
2022-09-29 12:29:19 -0500

    added start of importing a DB

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sqlite3
+from sqlite3 import Error
 from pydoc import render_doc
+from telnetlib import SB
 from xml.dom.minidom import Document
 from flask import Flask,render_template,request,flash
 from jinja2 import Template
@@ -8,58 +10,29 @@ from jinja2 import Template
 app = Flask(__name__)
 app.secret_key = b'_5asdsaw%2xec]/'
 
-db = 'dbs/finance.db'
 
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
 
+    return connection
 
+db = create_connection("dbs/finance.db")
+
+pointer = db.cursor()
+
+#sqlite_master is global hidden table
+#future you is going to hate dealing with this
+schema = pointer.execute("SELECT * FROM sqlite_master WHERE type='table';").fetchall()
 
 @app.route("/",methods = ['POST', 'GET'])
 def hello_world():
     if request.method == 'GET':
         table_counter = 0
-        #test = new_table(table_counter)
-        #template_test = Template(new_table(table_counter))
-        #return Template.render(template_test)
-    '''else:
-        
-        main_driver = request.form.get("main_driver")
-        warden = global_table_holder()
-        print ("MAIN ", main_driver)
-        try:
-            table_counter =  int(request.form.get("adj_tables_counter"))
-        except ValueError:
-            flash("bad int")
-            pass
-        
-        #test = new_table(table_counter)
-        #template_test = Template(new_table(table_counter))
-        #return Template.render(template_test)
-    final = "SELECT "
-    print ("METHOD ", request.method)
-    print ("COUTNER ", request.form.get("adj_tables_counter"))
-    print ("PREV ", request.form.get("prev_tables"))'''
-    
-    
-    #template_act = Template(new_table(str(table_counter+1)))
-    #return render_template("test.html",tables = template_act,meth = request.method, counter = str(table_counter+1))
-    #flash("test")
-    #flash("test2")
-    return render_template("starter.html",tables = new_table(table_counter),meth = request.method, tab_counter = table_counter,debug=True)
+    return render_template("starter.html",schema=schema,meth = request.method,debug=True)
         #return render_template("test.html",name = "test")
 
-
-
-def new_table(intake=0):
-        counter = 0
-        packer = {}
-        while counter < intake:
-            counter += 1
-            internal_name = "table" + str(counter)
-            internal_id = "table_id" + str(counter)
-            name = "table " + str(counter)
-            div_spacer = "div_" + str(counter)
-            internal_id_col = "id" + str(counter) + "_col"
-            table_data = "table_" + str(counter) + "_field"
-            col_data = "id" + str(counter) + "_col_data"
-            packer.update({counter:{"internal":internal_name,"internal_id":internal_name,"div_spacer":div_spacer,"internal_id":internal_id,"name":name,"internal_id_col":internal_id_col,"table_data":table_data,"col_data":col_data}})
-        return packer
