d203838e1cc19909b2f7b4386c3f51fc3f5ceb63
Ethel <34084693+Ethel173@users.noreply.github.com>
2022-10-05 18:16:43 -0500

    finished tables in rev2

--- static/rev2.js
+++ static/rev2.js
@@ -17,6 +17,7 @@
     }
 
     var table = {
+        unsafe_counter:0,
         built_obj : [],
         
         /*saved_names : [],
@@ -37,14 +38,43 @@
                 table.built_obj.push(names[asd])
                 table.built_obj[asd].push(cols[asd])
             }
-            //generic.debug()
+            console.log("debug in map")
+            generic.debug()
         },
 
         add:function(){
-            console.log("try add template")
-            var temp = document.getElementById("table_template")
-            var clon = temp.content.cloneNode(true)
-            document.getElementById("tab_content").appendChild(clon)
+            //console.log("try add template")
+            
+            //pull template, set ID, +1 counter
+            let templateInstance = document.getElementById("table_template");
+            let clone = document.importNode(templateInstance .content, true);
+
+            //its a reverse lookup you idiot
+            clone.querySelector('div.row').id = "table_"+table.unsafe_counter;
+            //add table data
+            //ugly repurpose of unsafe coutner as index access
+            // clone.querySelector('div.col').innerHTML = "Table : " + table.built_obj[table.unsafe_counter][0] + " Data: "+ table.built_obj[table.unsafe_counter][1];
+            //future me switch from inner, it purges childs
+            //or go down the rabbit hole again
+            clone.querySelector('div.col').innerHTML = "Table : " + table.built_obj[table.unsafe_counter][0];
+            var selbox_div = document.createElement("div")
+            selbox_div.setAttribute("class", "col")
+            var selectbox = document.createElement("select")
+            selectbox.setAttribute("id", "data"+table.unsafe_counter)
+
+            //past you has decided to go down the rabbit hole your not welcome
+            for (var item = 0; item < table.built_obj[table.unsafe_counter][1].length;item ++){
+                console.log(table.built_obj[table.unsafe_counter][1][item][1])
+                var option = document.createElement("option");
+                option.value = table.built_obj[table.unsafe_counter][1][item][1]
+                option.text = table.built_obj[table.unsafe_counter][1][item][1]
+                selectbox.appendChild(option);
+            }
+            
+            table.unsafe_counter +=1
+            selbox_div.appendChild(selectbox); 
+            clone.querySelector('div.row').appendChild(selbox_div); 
+            document.getElementById("tab_content").appendChild(clone); 
         },
 
 
